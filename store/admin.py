0a273de9eaf64b095ac3ec8aefd24669523d0ff9
Sherina Begin <98630481+SherinaBegin@users.noreply.github.com>
2022-04-26 15:35:58 -0700

    adding admin models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,35 @@
+from pyexpat import model
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection']
+    list_editable = ['unit_price']
+    list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'low'
+        return 'OK'
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'payment_status', 'customer']
+    list_per_page = 10
